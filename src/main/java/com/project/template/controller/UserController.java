127c3c78a90d1200464493919bee624d15ab701d
yande <gderudyartha@gmail.com>
2016-10-13 14:17:18 +0800

    update

--- src/main/java/com/project/template/controller/UserController.java
+++ src/main/java/com/project/template/controller/UserController.java
@@ -1,16 +1,25 @@
 package com.project.template.controller;
 
+import java.security.Principal;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class UserController {
 	private static Logger log = LoggerFactory.getLogger(UserController.class);
+	
+	@RequestMapping("/api/isAuthenticated")
+	@ResponseBody
+	public Principal user(Principal user) throws Exception {
+		return user;
+	}
 
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	@ExceptionHandler(Exception.class)
